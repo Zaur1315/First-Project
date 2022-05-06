321f739e2f0c049f1eb901baa1c83c6682a42163
zaur.quseynzade@gmail.com <zaur.quseynzade@gmail.com>
2022-05-06 20:53:46 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+"use scrict";
+
+
+// const result = confirm('are U here?'); //вопрос пользователю
+// console.log(result);
+// const question = prompt('How mutch?','18'); //вопрос с вводом, во торых ковычках вставляется шаблон.
+// console.log(question);
+
+
+const category = 'toys';
+console.log(`http://dsskdjeh/${category}`);
+
+
+const isCheck = true,
+    isClose = true;
+
